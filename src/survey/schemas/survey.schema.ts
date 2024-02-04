4939854de707ec37e43f162bbe43d4d8b2bd4183
mojjoiv <daltonongeche@gmail.com>
2024-02-04 17:04:44 +0300

    survey data captured

--- src/survey/schemas/survey.schema.ts
+++ src/survey/schemas/survey.schema.ts
@@ -3,7 +3,7 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 export enum Vaccination {
   YES = 'yes as a seperate service',
   NO = 'no',
-  CIRCUMSTANCE = 'only under special circumstancec',
+  CIRCUMSTANCE = 'only under special circumstance',
 }
 export enum Supply {
   YES = 'yes',
@@ -14,11 +14,110 @@ export enum Bonus {
   No = 'no',
   MAYBE = 'maybe',
 }
+export enum Cancer {
+  YES = 'yes as a seperate service',
+  No = 'no',
+  CIRCUMSTANCE = 'only under special circumstance',
+}
+
+export enum Sarea {
+  YES = 'yes',
+  No = 'no',
+}
+
+export enum Regions {
+  REGIONA = 'region A',
+  REGIONB = 'region B',
+  REGIONC = 'region C',
+  REGIOND = 'region D',
+}
+export enum Program {
+  YES = 'yes',
+  No = 'no',
+  MAYBE = 'maybe',
+}
+
+//hearing aid exports
+export enum HearingAid {
+  YES = 'yes as a seperate service',
+  No = 'no',
+  CIRCUMSTANCE = 'only under special circumstance',
+}
+
+export enum CompleteArea {
+  YES = 'yes',
+  No = 'no',
+}
+
+export enum Noregions {
+  REGIONA = 'region A',
+  REGIONB = 'region B',
+  REGIONC = 'region C',
+  REGIOND = 'region D',
+}
+
+export enum Service {
+  YES = 'yes',
+  No = 'no',
+  MAYBE = 'maybe',
+}
+
+//childbirth preparation exports
+export enum ChildBirth {
+  YES = 'yes as a seperate service',
+  No = 'no',
+  CIRCUMSTANCE = 'only under special circumstance',
+}
+
+export enum BirthArea {
+  YES = 'yes',
+  No = 'no',
+  MAYBE = 'maybe',
+}
+
+export enum Bregions {
+  REGIONA = 'region A',
+  REGIONB = 'region B',
+  REGIONC = 'region C',
+  REGIOND = 'region D',
+}
+
+export enum Bprogram {
+  YES = 'yes',
+  No = 'no',
+  MAYBE = 'maybe',
+}
+
+//childbirth exports
+export enum Foundation {
+  YES = 'yes as a seperate service',
+  No = 'no',
+  CIRCUMSTANCE = 'only under special circumstance',
+}
+
+export enum Csupply {
+  YES = 'yes',
+  No = 'no',
+}
+
+export enum Carea {
+  REGIONA = 'region A',
+  REGIONB = 'region B',
+  REGIONC = 'region C',
+  REGIOND = 'region D',
+}
+
+export enum Cprogram {
+  YES = 'yes',
+  No = 'no',
+  MAYBE = 'maybe',
+}
 
 @Schema({
   timestamps: true,
 })
 export class Survey {
+  //vaccination schema
   @Prop()
   vaccination: Vaccination;
 
@@ -33,6 +132,70 @@ export class Survey {
 
   @Prop()
   bonus: Bonus;
+
+  //skin cancer predetection schema
+  @Prop()
+  cancer: Cancer;
+
+  @Prop()
+  hften: string;
+
+  @Prop()
+  sarea: Sarea;
+
+  @Prop()
+  regions: Regions;
+
+  @Prop()
+  program: Program;
+
+  //hearing aid schema
+  @Prop()
+  hearingAid: HearingAid;
+
+  @Prop()
+  brand: string;
+
+  @Prop()
+  completearea: CompleteArea;
+
+  @Prop()
+  noregions: Noregions;
+
+  @Prop()
+  service: Service;
+
+  //childbirth preparation schema
+  @Prop()
+  childbirth: ChildBirth;
+
+  @Prop()
+  volume: string;
+
+  @Prop()
+  birtharea: BirthArea;
+
+  @Prop()
+  bregions: Bregions;
+
+  @Prop()
+  bprogram: Bprogram;
+
+  //childbirth schema
+  @Prop()
+  foundation: Foundation;
+
+  @Prop()
+  explain: string;
+
+  @Prop()
+  carea: Carea;
+
+  @Prop()
+  csupply: Csupply;
+
+  @Prop()
+  cprogram: Cprogram;
 }
 
 export const SurveySchema = SchemaFactory.createForClass(Survey);
